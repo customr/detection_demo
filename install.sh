d705729707132013beeb8af5b3dc6088bd0cb8c2
customr <shipicin_max@mail.ru>
2019-02-26 14:43:15 +0400

    fixes

--- install.sh
+++ install.sh
@@ -1,8 +1,10 @@
 python3 -m venv flask_env
 source flask_env/bin/activate
-pip3 install -U -r requirements.txt
+echo install requirements. Please wait.
+pip3 install -q -r requirements.txt
 export FLASK_ENV=flask_env
 cd app/src/yolov3
+echo load model weights
 bash model_weights/get_pretrained_coco_weights.sh
 cd ../../..
 python3 detection_demo.py
